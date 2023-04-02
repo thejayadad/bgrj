d6cdd1714fdb0e9b83f1f4746839f2f0f3d010f3
jaydunb12 <74875595+jaydunb12@users.noreply.github.com>
2023-04-02 19:22:50 -0400

    update

--- front/src/pages/Create.js
+++ front/src/pages/Create.js
@@ -1,8 +1,49 @@
 import React from 'react'
+import Navbar from '../components/Navbar'
+import Footer from '../components/Footer'
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import TextField from '@mui/material/TextField';
+
 
 const Create = () => {
   return (
-    <div>Create</div>
+    <div>
+      <Navbar />
+      <Box
+      sx={{
+        maxWidth:"900px",
+        margin: "auto",
+        height: "100vh"
+      }}>
+<Typography mt="50px" mb="50px" variant="h2" align="center">
+              New Post
+            </Typography>
+            <Box
+            sx={{
+              maxWidth: "700px",
+              margin: "auto",
+              padding: "20px"
+            }}
+            >
+              <form>
+              <TextField mb="20px" id="outlined-basic" label="Title" variant="outlined" fullWidth />
+              <TextField
+              fullWidth
+          id="outlined-multiline-static"
+          label="Description"
+          multiline
+          rows={4}
+          defaultValue="Default Value"
+        />
+
+              </form>
+
+            </Box>
+
+      </Box>
+      <Footer />
+      </div>
   )
 }
 
