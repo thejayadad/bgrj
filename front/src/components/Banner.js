2952788e1d9eca8069cc87578cff90b49cd5e0fc
jaydunb12 <74875595+jaydunb12@users.noreply.github.com>
2023-04-02 17:05:12 -0400

    update

--- front/src/components/Banner.js
+++ front/src/components/Banner.js
@@ -1,15 +1,40 @@
 import React from 'react'
 import Box from '@mui/material/Box';
 import CardMedia from '@mui/material/CardMedia';
+import Paper from '@mui/material/Paper';
+import Typography from '@mui/material/Typography';
 
 
 const Banner = () => {
   return (
     <div>
- <Box
+        <Paper
+        sx={{
+            component: "img",
+            height: 400,
+            backgroundImage: "https://images.pexels.com/photos/1707819/pexels-photo-1707819.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+        }}
+        >
+
+    <CardMedia
+        sx={{ height: "100%", objectFit: "coover" }}
+        image="https://images.pexels.com/photos/544115/pexels-photo-544115.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+        title="green iguana"
+      />
+
+        </Paper>
+
+    </div>
+  )
+}
+
+export default Banner
+
+
+{/* <Box
       sx={{
         width: "100%",
-        height: 700,
+        height: 400,
         // backgroundImage: 'image="https://images.pexels.com/photos/1707819/pexels-photo-1707819.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
         // '&:hover': {
         //   backgroundColor: 'primary.main',
@@ -23,10 +48,20 @@ const Banner = () => {
         title="green iguana"
       />
 
-    </Box>
+      <Box
+      sx={{
+        width: "350px",
+        background: "orange",
+        position: "absolute",
+        padding: 3,
+        top: 280,
+        left: 420
+      }}
 
-    </div>
-  )
-}
+      >
+        HERE IS IT
 
-export default Banner
+
+      </Box>
+
+    </Box> */}
