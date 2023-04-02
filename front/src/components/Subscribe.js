85cd06b05677a3c26f79a94db163c86a5cdff6fa
jaydunb12 <74875595+jaydunb12@users.noreply.github.com>
2023-04-02 19:01:36 -0400

    update

--- front/src/components/Subscribe.js
+++ front/src/components/Subscribe.js
@@ -10,9 +10,6 @@ const Subscribe = () => {
         <MarkEmailReadOutlinedIcon fontSize="large" />
       </IconButton>
       <Typography variant="h3">Subscribe To Our Newsletter</Typography>
-      <Typography>
-        and receive $20 coupon for your first order when you checkout
-      </Typography>
       <Box
         p="2px 4px"
         m="15px auto"
@@ -24,7 +21,7 @@ const Subscribe = () => {
         <InputBase
           sx={{ ml: 1, flex: 1 }}
           placeholder="Enter email"
-       
+
         />
         <Divider sx={{ height: 28, m: 0.5 }} orientation="vertical" />
         <Typography sx={{ p: "10px", ":hover": { cursor: "pointer" } }}>
