6772b7e5d9d779a37f3ba9e4aa566929be95416c
jaydunb12 <74875595+jaydunb12@users.noreply.github.com>
2023-04-02 15:34:42 -0400

    update

--- front/src/components/Banner.js
+++ front/src/components/Banner.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Box from '@mui/material/Box';
+import CardMedia from '@mui/material/CardMedia';
 
 
 const Banner = () => {
@@ -8,14 +9,21 @@ const Banner = () => {
  <Box
       sx={{
         width: "100%",
-        height: 500,
-        backgroundColor: 'primary.dark',
-        '&:hover': {
-          backgroundColor: 'primary.main',
-          opacity: [0.9, 0.8, 0.7],
-        },
+        height: 700,
+        // backgroundImage: 'image="https://images.pexels.com/photos/1707819/pexels-photo-1707819.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+        // '&:hover': {
+        //   backgroundColor: 'primary.main',
+        //   opacity: [0.9, 0.8, 0.7],
+        // },
       }}
-    />
+    >
+    <CardMedia
+        sx={{ height: "100%", objectFit: "coover" }}
+        image="https://images.pexels.com/photos/544115/pexels-photo-544115.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+        title="green iguana"
+      />
+
+    </Box>
 
     </div>
   )
