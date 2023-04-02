107ab5a0cb6add30aa6e7458c06b41f108ec644e
jaydunb12 <74875595+jaydunb12@users.noreply.github.com>
2023-04-02 13:44:35 -0400

    update

--- front/src/App.js
+++ front/src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+
+import {Routes, Route} from "react-router-dom"
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Create from "./pages/Create";
+import Update from "./pages/Update";
+import Detail from "./pages/Detail";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/create" element={<Create />} />
+        <Route path="/update/:id" element={<Update />} />
+        <Route path="/detail/:id" element={<Detail />} />
+      </Routes>
     </div>
   );
 }
---
+++ front/src/pages/Home.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import Navbar from '../components/Navbar'
+import Category from '../components/Category'
+import Footer from '../components/Footer'
+
+const Home = () => {
+  return (
+    <div>
+        <Navbar />
+        <Category />
+        <Footer />
+
+
+    </div>
+  )
+}
+
+export default Home
