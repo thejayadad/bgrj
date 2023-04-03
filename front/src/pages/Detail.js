54dc00a3b7ca51b52286cee83a0c5c21a4e30daa
jaydunb12 <74875595+jaydunb12@users.noreply.github.com>
2023-04-02 20:15:08 -0400

    update

--- front/src/pages/Detail.js
+++ front/src/pages/Detail.js
@@ -1,8 +1,29 @@
 import React from 'react'
+import Navbar from '../components/Navbar'
+import Footer from '../components/Footer'
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+
 
 const Detail = () => {
   return (
-    <div>Detail</div>
+    <div>
+      <Navbar />
+      <Typography mt="50px" mb="50px" variant="h2" align="center">
+              Post Detail
+      </Typography>
+      <Box
+           sx={{
+            maxWidth: "700px",
+            margin: "auto",
+            padding: "20px"
+          }}
+      >
+        content
+      </Box>
+
+      <Footer />
+      </div>
   )
 }
 
